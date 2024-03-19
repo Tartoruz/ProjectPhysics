4a3abaae044ed7019f783f7fda6cfdb634641b47
Tar toruz <tartoruz01@gmail.com>
2024-03-19 21:52:12 +0700

    Asset Added

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float moveSpeed;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
