8a4c92671245b55f32093bb8f560e3e0aea510ac
Opt <opttsorasit@gmail.com>
2024-03-23 23:28:57 +0700

    Moon Attraction

---
+++ Assets/Scripts/Gravity/Attraction.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attraction : MonoBehaviour
+{
+    [SerializeField] public float attractionForce = 10f;
+    [SerializeField] public float attractionRange = 5f;
+
+    private void FixedUpdate()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, attractionRange);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                Rigidbody playerRigidbody = collider.GetComponent<Rigidbody>();
+                if (playerRigidbody != null)
+                {
+                    Vector3 direction = transform.position - collider.transform.position;
+                    float distance = direction.magnitude;
+
+                    if (distance > 0)
+                    {
+                        float attractionStrength = attractionForce * 1000 / distance;
+                        Vector3 attraction = direction.normalized * attractionStrength;
+
+                        playerRigidbody.AddForce(attraction);
+                    }
+                }
+            }
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attractionRange);
+    }
+}
